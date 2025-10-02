b2ec793d54469612adb5a74ec04f0e5f15cb821a
MelodicAlbuild <ralexdrum@gmail.com>
2025-10-01 22:55:22 -0500

    feat: template

---
+++ apps/api/src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+// Routes will be added here during the live coding session
+
+app.listen(PORT, () => {
+  console.log(`Express API server listening on http://localhost:${PORT}`);
+});
